1d9eac2ff48008a3aa0f4492268d8aff39b753b8
thelmafrigita <thelmafrigita@student.telkomuniversity.ac.id>
2022-11-05 11:04:23 +0700

    MODUL1

---
+++ MODUL1/src/Database.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Database {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Selamat datang di Restoran EAD");
+        System.out.println("\nSilahkan Register Terlebih Dahulu");
+        System.out.println("\n=============================");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nama:");
+        System.out.println("No.Handphone:");
+        String oop = input.nextLine();
+    }
+}
